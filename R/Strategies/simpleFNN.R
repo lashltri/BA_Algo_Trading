236b2ffef64f29216a8678152fd2d579af2ec634
lashltri <126473345+lashltri@users.noreply.github.com>
2026-05-13 11:44:47 +0200

    Added ELM FUNCTION

--- R/Strategies/simpleFNN.R
+++ R/Strategies/simpleFNN.R
@@ -1,137 +1,11 @@
-# simple_FNN <- function(train_data, test_data,
-#                        number_neurons = c(12, 6),
-#                        num_sim = 10) {
-#   
-#   # preprocess ---------------------------------------------------------------
-#   feat_min <- apply(train_data, 2, min, na.rm = TRUE)
-#   feat_max <- apply(train_data, 2, max, na.rm = TRUE)
-#   
-#   scaled_train <- train_data
-#   scaled_test  <- test_data
-#   
-#   scaled_train[, -1] <- scale(scaled_train[, -1],
-#                               center = feat_min[-1],
-#                               scale  = feat_max[-1] - feat_min[-1])
-#   
-#   scaled_test[, -1] <- scale(scaled_test[, -1],
-#                              center = feat_min[-1],
-#                              scale  = feat_max[-1] - feat_min[-1])
-#   
-#   # initialize containers ----------------------------------------------------
-#   mse_runs <- matrix(NA, nrow = num_sim, ncol = 2)
-#   colnames(mse_runs) <- c("In sample MSE", "Out sample MSE")
-#   
-#   ret_is_runs <- xts(matrix(NA, nrow = nrow(train_data), ncol = num_sim),
-#                      order.by = index(train_data))
-#   ret_oos_runs <- xts(matrix(NA, nrow = nrow(test_data), ncol = num_sim),
-#                       order.by = index(test_data))
-#   
-#   signal_is_runs <- xts(matrix(NA, nrow = nrow(train_data), ncol = num_sim),
-#                         order.by = index(train_data))
-#   signal_oos_runs <- xts(matrix(NA, nrow = nrow(test_data), ncol = num_sim),
-#                          order.by = index(test_data))
-#   
-#   pred_oos_runs <- xts(matrix(NA, nrow = nrow(test_data), ncol = num_sim),
-#                        order.by = index(test_data))
-#   
-#   
-#   # simulation loop ----------------------------------------------------------
-#   cat("i |", paste(colnames(mse_runs), collapse = " | "), "\n")
-#   for (i in 1:num_sim) {
-#     
-#     nn_obj <- try(
-#       neuralnet(paste(colnames(scaled_train)[1], "~ ."),
-#                 data = scaled_train,
-#                 hidden = number_neurons,
-#                 linear.output = TRUE),
-#       silent = TRUE
-#     )
-#     
-#     if (class(nn_obj) == "try-error" || is.null(nn_obj$net.result[[1]])){next}
-#     
-#     pred_is <- nn_obj$net.result[[1]]
-#     pred_oos_runs[, i] <- predict(nn_obj, as.matrix(scaled_test[, -1]))
-#     
-#     mse_runs[i, 1] <- mean((scaled_train[, 1] - pred_is)^2)
-#     mse_runs[i, 2] <- mean((scaled_test[, 1] - pred_oos_runs[, i])^2)
-#     
-#     ret_is_runs[, i]   <- (pred_is > 0) * train_data[,1]
-#     ret_oos_runs[, i]  <- (pred_oos_runs[, i]  > 0) * test_data[,1]
-#     
-#     signal_is_runs[, i]  <- (pred_is > 0)
-#     signal_oos_runs[, i] <- (pred_oos_runs[, i]  > 0)
-#     
-#     
-#     cat(i, "|", paste(round(mse_runs[i, ], 8), collapse = " | "), "\n")
-#   }
-#   
-#   
-#   # combine successful runs --------------------------------------------------
-#   
-#   ret_oos_avg <- xts(apply(ret_oos_runs, 1, mean), order.by = index(test_data))
-#   signal_oos_avg <- xts(apply(signal_oos_runs, 1, mean), order.by = index(test_data))
-#   
-#   # summary ------------------------------------------------------------------
-#   cat("Mean IS MSE:", mean(mse_runs[, 1], na.rm = TRUE),
-#       "| Mean OOS MSE:", mean(mse_runs[, 2], na.rm = TRUE), "\n")
-#   cat("Sd IS MSE:", sd(mse_runs[, 1], na.rm = TRUE),
-#       "| Sd OOS MSE:", sd(mse_runs[, 2], na.rm = TRUE), "\n")
-#   
-#   
-#   # Sanity Check ----------------------------------------------------------------
-#   # if (!is.null(ret_is_runs) && !is.null(ret_oos_runs)) {
-#   #   par(mfrow = c(1, 2))
-#   #   plot(cbind(cumsum(ret_is_runs), cumsum(train_data[,1])),
-#   #        col = c(1:ncol(ret_is_runs), 1),
-#   #        lwd = c(rep(1, ncol(ret_is_runs)), 3),
-#   #        main = "In-Sample Performance",
-#   #        ylab = "Cumulative Return")
-#   #   plot(cbind(cumsum(ret_oos_runs), cumsum(test_data[,1])),
-#   #        col = c(1:ncol(ret_oos_runs), 1),
-#   #        lwd = c(rep(1, ncol(ret_oos_runs)), 3),
-#   #        main = "Out-of-Sample Performance",
-#   #        ylab = "Cumulative Return")
-#   # }
-#   
-#   
-#   
-#   return(list(return = ret_oos_avg,
-#               signal = signal_oos_avg,
-#               
-#               return_out_mat = ret_oos_runs,
-#               predicted_oos_mat = pred_oos_runs))
-# }
-
-
-
-
-
-
-
-# predict_nn <- function(x_new, updated_params, x_train_ref, y_train_ref,
-#                                 neuron_vec, linear_output = TRUE,
-#                                 atan_not_sigmoid = FALSE) {
-#   
-#   layer_size <- getLayerSize(x_train_ref, y_train_ref, neuron_vec)
-#   
-#   fwd <- forwardPropagation(
-#     x_train = as.matrix(x_new),
-#     params = updated_params,
-#     list_layer_size = layer_size,
-#     linear_output = linear_output,
-#     atan_not_sigmoid = atan_not_sigmoid
-#   )
-#   
-#   as.vector(fwd$A_list[[length(fwd$A_list)]])
-# }
-
 
 
 simple_FNN_wildi <- function(train_data, test_data,
                                 number_neurons = c(100),
-                                num_sim = 10,
-                                epochs = 200,
-                                learning_rate = 0.3) {
+                                num_sim = 100,
+                                epochs = 100,
+                                learning_rate = 0.3,
+                                ELM = FALSE) {
   
   # preprocess ---------------------------------------------------------------
   feat_min <- apply(train_data, 2, min, na.rm = TRUE)
@@ -187,6 +61,41 @@ simple_FNN_wildi <- function(train_data, test_data,
       silent = TRUE
     )
     layer_size <- getLayerSize(x_train, y_train, number_neurons)
+    
+    if (ELM == TRUE) {
+      params_el <- nn_obj$updated_params
+      
+      cache_hidden <- forwardPropagation(
+        x_train = x_train,
+        params = params_el,
+        list_layer_size = layer_size,
+        linear_output = TRUE,
+        atan_not_sigmoid = FALSE
+      )
+      
+      H <- t(cache_hidden$A_list[[length(layer_size$n_h)]])
+      y <- as.numeric(y_train)
+      
+      fit_el <- lm(y ~ H)
+      coef_el <- coef(fit_el)
+      coef_el[is.na(coef_el)] <- 0
+      
+      params_el$W_list[[length(layer_size$n_h) + 1]] <- matrix(coef_el[-1], nrow = 1)
+      params_el$b_list[[length(layer_size$n_h) + 1]] <- matrix(coef_el[1], nrow = 1)
+      
+      nn_obj$updated_params <- params_el
+      
+      cache_el <- forwardPropagation(
+        x_train = x_train,
+        params = nn_obj$updated_params,
+        list_layer_size = layer_size,
+        linear_output = TRUE,
+        atan_not_sigmoid = FALSE
+      )
+      
+      cost_el <- computeCost(y_train, cache_el)
+      nn_obj$cost_hist[length(nn_obj$cost_hist)] <- cost_el
+    }
     
     cache_is <- forwardPropagation(
       x_train = x_train,
